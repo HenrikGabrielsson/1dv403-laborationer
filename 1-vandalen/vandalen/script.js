c8259cb1335a21722535b97c1fbacc9c187dfd85
OpenShift guest <hg222aa@student.lnu.se>
2013-11-21 05:19:32 -0500

    vandalen klar

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -5,8 +5,35 @@ var makePerson = function(persArr){
     //objektet som ska returneras.
 	var personData = new Object();
 	
-	persArr.sort();
+	//Egenskaper
+	personData.names = "";
+	personData.maxAge = 0;
+	personData.minAge = 0;
+	personData.averageAge = 0;
+
+	var sortedArrName = [];
+	var sortedArrAge = [];
 	
+	//Loopar igenom argumentsarrayen
+	for(var i = 0; i < persArr.length; i++)
+	{
+	    personData.averageAge += (persArr[i].age);
+	    sortedArrName[i] = persArr[i].name;
+	    sortedArrAge[i] = persArr[i].age;
+
+
+	//sorterar arrayer med namn respektive åldrar
+	sortedArrName = sortedArrName.sort(function(a,b){return a.toString().localeCompare(b.toString())});
+	sortedArrAge = sortedArrAge.sort(function(a,b){return a-b});
+	
+	
+	//Skapar strängen med namnen
+	personData.names = sortedArrName.join(", ");
+	
+	//tilldelar egenskaperna för åldrar sina 
+	personData.minAge = sortedArrAge[0];
+	personData.maxAge = sortedArrAge[(sortedArrAge.length-1)];
+	personData.averageAge = Math.round(personData.averageAge /= persArr.length);
+
     return personData;
 };
-
