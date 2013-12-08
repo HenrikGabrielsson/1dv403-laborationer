b8bd148a8a6b151817bfbe4109059f302fbfff7a
OpenShift guest <hg222aa@student.lnu.se>
2013-12-08 08:42:05 -0500

    nu skapas rutor där korten ska vara

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -3,27 +3,25 @@
 var Memory = 
 {
     
+    game_board: document.getElementById("game_board"),
+    
     //Här sparas antalet gissningar
     guesses: 0,
-    
-    boardHeight:0,
-    boardWidth:0,
-    
+
+    memoryBoard: [],
     
     init:function () 
     {
 
         //skapar ett nytt spelbräde
         var submit = document.getElementById("submit_button");
-        submit.addEventListener("click",Memory.getBoardSize, false);
-        
-        var memoryBoard = new RandomGenerator.getPictureArray(Memory.boardHeight, Memory.boardWidth);
+        submit.addEventListener("click",Memory.getInput, false);
         
     },  
     
     
     //funktion som hämtar spelbrädets storlek från användaren.    
-    getBoardSize: function()
+    getInput: function()
     {
         var errorP = document.getElementById("error_message");
         
@@ -35,23 +33,53 @@ var Memory =
         {
             errorP.innerHTML = "Du måste skriva siffror.";    
         }
-        else if((height > 8 || height < 1 ) || (width > 8 || width < 1 ))
+        else if( height < 1 || width < 1 )
         {
-            errorP.innerHTML = "Höjden och bredden måste vara mellan 1-8 brickor.";          
+            errorP.innerHTML = "Både höjden och bredden måste vara minst 1 bricka";          
         }
+        
+        else if(width * height > 16 )
+        {
+            errorP.innerHTML = "Tyvärr, du får inte ha mer än 16 brickor på spelplanen.";    
+        }
+        
         else if( (width*height) % 2 !== 0)
         {
             errorP.innerHTML = "Tyvärr, du måste ha ett jämt antal brickor på planen.";    
         }
+
         else
         {
-            errorP.innerHTML = null;  
-            Memory.boardHeight = height;
-            Memory.boardWidth = width;
+            errorP.innerHTML = null; 
+            
+            Memory.createBoard(height, width);
         }
 
         
     },
+    
+    //funktion som skapar spelbrädet
+    createBoard: function(height,width)
+    {
+            //här skapas spelplanen  
+            var memoryBoard = new RandomGenerator.getPictureArray(height, width);
+            
+
+            
+            for(var i = 0; i < memoryBoard.length; i++)
+            {
+                var card = document.createElement("div");
+                card.setAttribute("class","card");
+                
+                if(i % width === 0)
+                {
+                card.setAttribute("class","card new_row_card");
+                }
+                
+                Memory.game_board.appendChild(card);
+            }
+            
+    }
 };
 
 window.onload = Memory.init;
