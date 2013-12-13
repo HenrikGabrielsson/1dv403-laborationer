b045ebd31103fc212ad0edb67c8c1d08e56250cc
OpenShift guest <hg222aa@student.lnu.se>
2013-12-13 08:21:17 -0500

    alla värden hämtas från formuläret nu

--- 4-forms/script/validator.js
+++ 4-forms/script/validator.js
@@ -4,8 +4,26 @@ var Validator =
 {
     init: function()
     {
+        var form = document.getElementById("formElement");
+        form.onsubmit = Validator.validate;
+    },
+    
+    validate: function()
+    {
+        //hämtar de olika fälten som ska testas
+        var firstName = document.getElementsByName("firstName")[0].value;
+        var lastName = document.getElementsByName("lastName")[0].value;
+        var postNumber = document.getElementsByName("postNumber")[0].value;
+        var eMail = document.getElementsByName("eMail")[0].value;
+        var selectedPrice = document.getElementsByName("price")[0].selectedIndex;
+        var price = document.getElementsByName("price")[0].options;
+        //price[selectedPrice] ger valet från dropdown-menyn
+        
+
+        
+
     }
-}
+};
 
 window.onload = Validator.init();
 
